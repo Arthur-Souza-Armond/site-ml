7adcff1b03af8a97bf91bd7bb407907549bc7f64
ZzSouzazZ <asarmond82@gmail.com>
2023-01-05 22:55:06 -0300

    version 1.0.1

--- pages-templates/page-treinamento.php
+++ pages-templates/page-treinamento.php
@@ -59,7 +59,7 @@
                                 01
                             </h3>
                             <p>
-                                Quer <a>vender online</a> e ter um <a>negócio digital</a>
+                                Quer <a>vender no Mercado Livre</a> e não sabe por onde <a>começar</a>
                             </p>
                         </div>
                         <img>                                        
@@ -70,7 +70,7 @@
                                 02
                             </h3>
                             <p>
-                                <a>Já vende</a> no Mercado Livre e quer <a>acelerar seus resultados</a>
+                                <a>Já vende</a> no Mercado Livre e quer alcançar o tão sonhado <a>Platinum</a>
                             </p>
                         </div>   
                         <img>                     
@@ -83,7 +83,7 @@
                                 03
                             </h3>
                             <p>
-                                Já <a>conhece o mercado digital</a> e quer aprender a <a>vender mais</a>
+                                Já tem <a>loja física</a> e quer trazer para o <a>online</a>
                             </p>
                         </div>                        
                         <img> 
@@ -94,7 +94,7 @@
                                 04
                             </h3>
                             <p>
-                                Quer conhecer o método <a>MV30</a> e fazer vendas diárias no ML
+                                Quer entrar para a <a>comunidade e-com</a> e ter todos os <a>benefícios</a>
                             </p>
                         </div>                        
                         <img> 
@@ -114,58 +114,112 @@
                 </h4>
                 <div class="div-acordeon" onclick="show_selected_accordeon( 'div-acordeon-modulo1' )">
                     <h3>
-                        01 - Primeiros passos
+                        01 - Seu futuro começa aqui
                     </h3>
                     <div id="div-acordeon-modulo1">
-                        Esse módulo será dado os primeiros passos sobre criação de conta e boas vindas aos novos alunos
+                        Aqui eu vou te dar uma visão do que é um mercado de vendas online e a grande oportunidade que ele vai trazer para sua vida financeira e profissional
                     </div>
                 </div>
                 <div class="div-acordeon" onclick="show_selected_accordeon( 'div-acordeon-modulo2' )">
                     <h3>
-                        02 - Conhecendo a plataforma
+                        02 - Contrua um negócio sólido
                     </h3>
                     <div id="div-acordeon-modulo2">
-                        Neste módulo você conhecerá a plataforma do Mercado Livre na visualização de vendedor. Aprenderá a emitir notas, verificar pedidos, gerenciar anúncios e muito mais
+                        Nesse módulo, eu vou te mostrar o que você deve fazer e o que você não deve fazer para não construir um castelo de areia.
                     </div>
                 </div>
                 <div class="div-acordeon" onclick="show_selected_accordeon( 'div-acordeon-modulo3' )">
                     <h3>
-                        03 - Anúncios
+                        03 - Finanças para E-commerce
                     </h3>
                     <div id="div-acordeon-modulo3">
-                        Neste módulo você aprenderá a fazer um anúncio de primeira página com alta qualidade, boas imagens, boa descrição visando ao máximo as conversões.<br><br>Aprenda a produzir imagens de qualidade para os anúncios e como alcançar mais pessoas
+                        Neste módulo, eu vou te falar sobre quais as melhores formas de você comprar produtos e também sobre o que é o seu dinheiro e o que é o dinheiro da empresa
                     </div>
                 </div>
                 <div class="div-acordeon" onclick="show_selected_accordeon( 'div-acordeon-modulo4' )">
                     <h3>
-                        04 - Fornecedores
+                        04 - Primeiros passos no Mercado Livre
                     </h3>
                     <div id="div-acordeon-modulo4">
-                        Você aprenderá como encontrar bons fornecedores para os produtos que deseja e também a encontrar produtos quentes para fazer suas vendas
+                        Nesse módulo eu vou te ensinar como fazer as primeiras configurações de forma correta para você não ter nenhum problema futuro, vou te mostrar como funciona a plataforma do mercado livre, e o que você precisa para começar
                     </div>
                 </div>
                 <div class="div-acordeon" onclick="show_selected_accordeon( 'div-acordeon-modulo5' )">
                     <h3>
-                        05 - Reputação
+                        05 - Nichos e produtos
                     </h3>
                     <div id="div-acordeon-modulo5">
-                        Você aprenderá a entender como funciona a reputação da sua conta e como manter bons números para ser recomendado a mais usuários
+                        Nesse módulo, eu vou te ensinar a achar o ouro do negócio, vou quebrar os paradigmas sobre nichos e te ensinar a encontrar os produtos para você faturar alto.
                     </div>
                 </div>
                 <div class="div-acordeon" onclick="show_selected_accordeon( 'div-acordeon-modulo6' )">
                     <h3>
-                        06 - Shopee
+                        06 - Fornecedores
                     </h3>
                     <div id="div-acordeon-modulo6">
-                        Nesse módulo bônus você aprenderá um pouco sobre o ecossistema da plataforma Shopee e como pode montar um negócio de sucesso nesta plataforma também
+                        Nesse módulo, vou te ensinar a encontrar qualquer fornecedor do Brasil e também ensinar como fazer a negociar com qualquer forncedor de uma forma que você sempre consiga comprar os produtos nas melhores condições
                     </div>
                 </div>
                 <div class="div-acordeon" onclick="show_selected_accordeon( 'div-acordeon-modulo7' )">
                     <h3>
-                        07 - Tiny
+                        07 - Precificação de produtos
                     </h3>
                     <div id="div-acordeon-modulo7">
-                        Neste módulo bônus você aprenderá a usar um sistema de ERP muito útil para gerenciar seu negócio e conseguir faturar mais
+                        Nesse módulo, eu vou te ensinar a analisar e precificar os produtos da maneira correta para que você tenha lucro e não prejuízo. T
+                    </div>
+                    <h3>
+                        08 - Fotos para anúncios
+                    </h3>
+                    <div id="div-acordeon-modulo7">
+                        Nesse módulo, eu vou te ensinar como fazer seu anúncio vender muito mais com fotos que convertem de maneira simples e rápidas.
+                    </div>
+                    <h3>
+                        09 - Anúncios Spartanos
+                    </h3>
+                    <div id="div-acordeon-modulo7">
+                        Nesse módulo, você vai aprender como faz um anúncio muito poderoso que irá aumentar a visibilidade em pelo menos 10x mais. Você vai aprender cada passo, de forma detalhada, para dominar as primeiras páginas.
+                    </div>
+                    <h3>
+                        10 - Modalidades de envios
+                    </h3>
+                    <div id="div-acordeon-modulo7">
+                        Nesse módulo, você vai aprender quais as características das modalidades de envio e qual a melhor estratégia para alavancar o posicionamento dos seus anúncios usando a logística de entrega do Mercado Livre.
+                    </div>
+                    <h3>
+                        11 - 10 Primeiras vendas
+                    </h3>
+                    <div id="div-acordeon-modulo7">
+                        Nesse módulo, você vai aprender 2 estratégias para quebrar a barreira das 10 primeiras vendas e ativar sua reputação de forma rápida.
+                    </div>
+                    <h3>
+                        12 - Vendi e agora?
+                    </h3>
+                    <div id="div-acordeon-modulo7">
+                        Nesse módulo, vou te ensinar o passo a passo para você fazer o envio da sua venda com sucesso.
+                    </div>
+                    <h3>
+                        13 - Validando produtos
+                    </h3>
+                    <div id="div-acordeon-modulo7">
+                        Nesse módulo, vou te ensinar sobre o que é um produto validado e a hora que você deve fazer profundidade de estoque para não correr o risco de acabar produto e você ter que pausar o anúncio.
+                    </div>
+                    <h3>
+                        14 - Multiplicando anúncios
+                    </h3>
+                    <div id="div-acordeon-modulo7">
+                        Nesse módulo, vou te ensinar a estratégia de milhões,  aqui está o caminho do seu primeiro milhão faturado.
+                    </div>
+                    <h3>
+                        15 - Posicionando anúncios na primeira página
+                    </h3>
+                    <div id="div-acordeon-modulo7">
+                        Nesse módulo, vou te ensinar 3 estratégias poderosas para você alcançar o sonho de todos os vendedores no Mercado Livre: <strong>posicionar produtos na primeira página</strong>
+                    </div>
+                    <h3>
+                        16 - Mercado ADS
+                    </h3>
+                    <div id="div-acordeon-modulo7">
+                        Nesse módulo, vou te ensinar as melhores estratégias para você dominar a ferramenta do Mercado ADS e dobrar seu faturamento.
                     </div>
                 </div>
                 <button onclick="ir_para_hotmart()">QUERO VENDER ONLINE</button>
@@ -234,21 +288,39 @@
                 <h4>Bônus</h4>
                 <div>
                     <span>BÔNUS</span>
-                    <h3>ERP</h3>
-                    <a>Com Tiny</a>
-                    <p>Integração com o ERP da Olist que gerencia seus produtos, anúncios, finanças e muito mais</p>
+                    <h3>Google Trends</h3>
+                    <a>Ferramenta Google</a>
+                    <p>Aqui você vai aprender como encontrar os termos mais buscados do seu produto pelos clientes na internet</p>
                 </div>
                 <div>
                     <span>BÔNUS</span>
-                    <h3>Impostos</h3>
+                    <h3>Ubersuggest</h3>
+                    <a>Ferramenta</a>
+                    <p>Aqui você vai aprender a encontrar as melhores palavras chaves para você poder usar no título do anúncio</p>
+                </div>
+                <div>
+                    <span>BÔNUS</span>
+                    <h3>Meli PRO</h3>
+                    <a>Ferramenta</a>
+                    <p>Aqui você vai aprender a analisar informações valiosas dos seus concorrentes e também análise de mercado</p>
+                </div>
+                <div>
+                    <span>BÔNUS</span>
+                    <h3>Nubmetrics</h3>
+                    <a>Ferramenta</a>
+                    <p>Aqui você vai aprender a analisar produtos em alta, anúncios em alta, ranqueamento de anúncios e muito mais</p>
+                </div>
+                <div>
+                    <span>BÔNUS</span>
+                    <h3>Tiny</h3>
+                    <a>ERP</a>
+                    <p>Aqui você vai aprender a aintegrar os seus anúncios nos principais marketplaces de maneira automática, gerenciando de forma simples e muito lucrativa</p>
+                </div>
+                <div>
+                    <span>BÔNUS</span>
+                    <h3>Contabilidade para E-commerce</h3>
                     <a>com especialista</a>
-                    <p>Com a ajuda de um especialista em tributação, fizemos uma aula especial para ensinar a vocês sobre impostos e sistema fiscal</p>
-                </div>
-                <div>
-                    <span>BÔNUS</span>
-                    <h3>FORNECEDORES</h3>
-                    <a>verificados</a>
-                    <p>Lista com fornecedores verificados e que vão fornecer os produtos que for vender</p>
+                    <p>Aqui você vai aprender os princípios de contabilidade fiscal e terá acesso a uma contabilidade especial para E-commerce com benefícios exclusivos</p>
                 </div>
             </div>
             <div class="precos-treinamento">
@@ -259,15 +331,18 @@
                 <p>
                     Ter a liberdade de escolher o que quer fazer e trabalhar para construir algo de valor para você mesmo é impagável! Vendendo nas plataformas digitais você pode trabalhar de qualquer lugar e viver apenas com a venda dos seus produtos
                 </p>
-                <span>DE <a>R$ 497,00</a> POR APENAS</span><br>
+                <span>DE <a>R$ 997,00</a> POR APENAS</span><br>
                 <div>
-                    <span>10x de R$ <a>24,75</a></span>
+                    <span>11x de R$ <a>45,18</a></span>
                     <p>
-                        À vista por R$ 297,00
+                        À vista por R$ 497,00
                     </p>
                 </div>
                 <button onclick="ir_para_hotmart()">QUERO VENDER ONLINE</button>
                 <img decoding="async" width="90%" height="32" src="https://wpfast.com.br/wp-content/uploads/2022/08/fda-pagamentos.svg" style="margin: 2% 5%">
+            </div>
+            <div class="sobre-mim">
+                    
             </div>
         </main>
         <script id="script-lp" type="text/javascript" src="<?php echo get_theme_file_uri() . '/pages-templates/treinamento/script.js' ?>"></script>
